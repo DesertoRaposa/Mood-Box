55668bee6d7ff66cd5685fe8b97cf27d503935b0
Rezeda Khairullina <deserto.raposa@yandex.ru>
2020-12-17 17:26:44 +0300

    Added Boostrap 4

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,9 @@ import { MoodCard } from './MoodCard.js';
 import { MoodCardsList } from './MoodCardsList.js';
 import { myRequest } from './myRequest.js';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 const moodCardsList = new MoodCardsList();
 const myReq = new myRequest ('http://localhost:3000/initialCards');
 
